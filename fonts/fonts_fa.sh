7cb0f390457b1992e23b63f6f146b210ca474483
Ali Gholami Rudi <ali@rudi.ir>
2025-11-20 21:22:03 +0330

    fonts_fa.sh: move fonts to dev.rudi.ir

--- fonts/fonts_fa.sh
+++ fonts/fonts_fa.sh
@@ -6,13 +6,13 @@ HGET="wget -c -O"
 
 # IR fonts
 #FONTURL_IR="http://scict.ir/Portal/File/ShowFile.aspx?ID=8964a122-b392-4261-9dd5-10c1938f0c8a"
-FONTURL_IR="https://neatroff.pages.dev/irfonts.tar.gz"
+FONTURL_IR="https://dev.rudi.ir/irfonts.tar.gz"
 echo "Retrieving $FONTURL_IR"
 $HGET irfonts.tar.gz $FONTURL_IR || exit 1
 tar xzf irfonts.tar.gz && mv irfonts/*.ttf . && rm -r irfonts/
 # B fonts
 #FONTURL_B="http://bayanbox.ir/domain/irfont.ir/Fonts/BFonts.zip?download"
-FONTURL_B="https://neatroff.pages.dev/bfonts.tar.gz"
+FONTURL_B="https://dev.rudi.ir/bfonts.tar.gz"
 echo "Retrieving $FONTURL_B"
 $HGET bfonts.tar.gz $FONTURL_B || exit 1
 tar xzf bfonts.tar.gz && mv bfonts/*.ttf . && rm -r bfonts/
